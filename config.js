95f7fe5fadfa5deb96fa2196e1e844afcdb9b28f
a0980778082-coder <a0980778082@gmail.com>
2026-09-23 01:47:15 +0800

    Configure live Cloud Run LINE Pay gateway

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 window.SAVAGE_CONFIG = {
   API_URL: 'https://script.google.com/macros/s/AKfycbzCdzZvvf0WUZ_sTvK-UWXwgjlCWLZaUfc4AcfsV4l6pbn-WF7NrZUxVdHit1SvY1LG/exec',
+  LINEPAY_API_URL: 'https://savage-linepay-wpem775bea-de.a.run.app',
   BRAND_NAME: '小野人餐盒製造所',
   ORDER_TITLE: '百貨專屬點餐'
 };
